4f70846daebb79c142e825f28f370ac957c15cb8
IlTuoNome <iltuo@email.com>
2025-06-30 19:09:25 +0200

    primo commit

--- My_Plants/urls.py
+++ My_Plants/urls.py
@@ -1,8 +1,9 @@
-# urls.py
+
 from django.urls import path
 from MyPlants import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('add-plant/', views.add_plant_view, name='add_plant'),
 ]
