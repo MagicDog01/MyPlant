dd534d463bdc2f27689da168f567eed8dbd420c7
IlTuoNome <iltuo@email.com>
2025-06-30 18:11:51 +0200

    primo commit

--- MyPlants/views.py
+++ MyPlants/views.py
@@ -1,5 +1,19 @@
-
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 def login_view(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")  # Sostituisci con la tua home
+        else:
+            return render(request, "login.html", {"error": "Credenziali non valide"})
+    return render(request, "login.html")
+
+
+
+def signup_view(request):
+    return render(request, 'signup.html')
