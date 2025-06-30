4a3426fa8155700e623b9382213cf102898109c0
DeLucia092 <vincenzo.delucia003@studenti.uniparthenope.it>
2025-06-30 19:12:11 +0200

    commit vincenzo

--- MyPlants/views.py
+++ MyPlants/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
-def login_view(request):
+def login(request):
     if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
